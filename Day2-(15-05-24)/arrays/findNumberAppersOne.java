7edc98f8c472864adbd836779ddaf0dcd4fa84c2
Puskar-Roy <puskarroy600@gmail.com>
2024-05-15 09:57:56 +0530

    feat : appers one

---
+++ Day2-(15-05-24)/arrays/findNumberAppersOne.java
@@ -0,0 +1,10 @@
+
+public class findNumberAppersOne {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+        return xor;
+    } 
+}
